7c10ed22656f51bfa3cd06e3851e797a2447c51a
TBuscemi <thomasbuscemi124@gmail.com>
2021-11-13 21:06:33 -0500

    Add some pictures for refrences and for the Website/now created scss which made 2 addtion for each folder.
    Co-authored-by: Midnight <Midnight-Sighs@users.noreply.github.com>

---
+++ veteran_assistance/src/components/mission/Mission.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+class Mission extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div>
+                Mission
+            </div>
+         );
+    }
+}
+ 
+export default Mission;
--- veteran_assistance/src/components/routerNav/RouterNav.jsx
+++ veteran_assistance/src/components/routerNav/RouterNav.jsx
@@ -1,6 +1,11 @@
 import { Nav, Navbar, NavbarBrand} from 'react-bootstrap';
 import React, { Component } from 'react';
 import { Routes, Route} from "react-router-dom";
+import Home from '../home/Home';
+import About from '../about/About';
+import Article from '../articles/Articles';
+import Contact from '../contact/Contact';
+import Mission from '../mission/Mission';
 
 class RouterNav extends Component {
     render() { 
@@ -10,11 +15,19 @@ class RouterNav extends Component {
                     <NavbarBrand href=""> Test! </NavbarBrand>
                         <Nav> 
                             <Nav.Item><Nav.Link id="textLinks" href="/">Home</Nav.Link></Nav.Item>
+                            <Nav.Item><Nav.Link id="textLinks" href="about">About</Nav.Link></Nav.Item>
+                            <Nav.Item><Nav.Link id="textLinks" href="mission">Mission</Nav.Link></Nav.Item>                            
+                            <Nav.Item><Nav.Link id="textLinks" href="article">Article</Nav.Link></Nav.Item>
+                            <Nav.Item><Nav.Link id="textLinks" href="contact">Contact</Nav.Link></Nav.Item>
                         </Nav>
                     </Navbar>
 
                     <Routes>
-                        <Route></Route>
+                        <Route  path='/' element  ={<Home/>}></Route>
+                        <Route  path='about' element  ={<About/>}></Route>
+                        <Route  path='article' element  ={<Article/>}></Route>
+                        <Route  path='contact' element  ={<Contact/>}></Route>
+                        <Route  path='mission' element  ={<Mission/>}></Route>
                     </Routes>
             </div>
         )
