bf161ee43104323b787d12034a46a9e0caea45e4
TBuscemi <thomasbuscemi124@gmail.com>
2021-12-07 17:31:33 -0500

    finished up the contract form

--- veteran_assistance/src/components/getStarted/GetStarted.jsx
+++ veteran_assistance/src/components/getStarted/GetStarted.jsx
@@ -13,13 +13,13 @@ render() {
             <div class="form-container">
                 <h1>BEEP BOOP INFO PLS</h1>
                 <form className="form" method="POST" action="https://formsubmit.co/polarbear494@gmail.com" enctype="multipart/form-data">
-                    <input type="First Name" name="First Name" placeholder="First Name" required/>
-                    <input type="Last Name" name="Last Name" placeholder="Last Name" required/>
-                    <input type="Email" name="Email" placeholder="Your email" required/>
-                    <input type="LinkedIn" name="LinkedIn" placeholder="LinkedIn"required/>
-                    <input type="Phone Number" name="Phone Number" placeholder="Phone Number " required/>
-                    <label for="Military Service">Military Service:</label>
-                    <select id="Military Service" name="Military Service" required>
+                    <input className="inputs" type="First Name" name="First Name" placeholder="First Name" required/>
+                    <input className="inputs" type="Last Name" name="Last Name" placeholder="Last Name" required/>
+                    <input className="inputs" type="Email" name="Email" placeholder="Your Email" required/>
+                    <input className="inputs" type="LinkedIn" name="LinkedIn" placeholder=" YourLinkedIn"required/>
+                    <input className="inputs"type="Phone Number" name="Phone Number" placeholder="Phone Number " required/>
+                    <label for="Military Service">Military Service <br</br> :</label>
+                    <select className="inputs" id="Military Service" name="Military Service" required>
                       <option value="Army">Army</option>
                       <option value="Navy">Navy</option>
                       <option value="Air Force">Air Force</option>
@@ -27,10 +27,10 @@ render() {
                       <option value="Coast Guard">Coast Guard</option>
                       <option value="None">None</option>
                     </select>
-                    <input type="MOS/Rating" name="MOS/Rating" placeholder="MOS/Rating/Specialty Code"/>
-                    <input type="Years of service" name="Years of service" placeholder="Years of service"/>
-                    <input type=" Honorable Discharge" name=" Honorable Discharge" placeholder=" Honorable Discharge"/>
-                    <input type="file" name="attachment" accept="pdf, pdf"/>
+                    <input className="inputs" type="MOS/Rating" name="MOS/Rating" placeholder="MOS/Rating/Specialty Code"/>
+                    <input className="inputs" type="Years of service" name="Years of service" placeholder="Years of service"/>
+                    <input className="inputs" type=" Honorable Discharge" name=" Honorable Discharge" placeholder=" Honorable Discharge"/>
+                    <input className="inputs" type="file" name="attachment" accept="pdf, pdf"/>
                     <input type="hidden" name="_next" value="http://localhost:3000/gets-started"/>
                     <button type="submit">Submit</button>
                 </form>
